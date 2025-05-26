dec1a43b36405c0db4739aba00211148c258eb09
Radiump123 <radiump123@gmail.com>
2025-05-26 18:02:04 -0400

    Create memory.c

---
+++ kernel/memory.c
@@ -0,0 +1,9 @@
+// memory.c — fake-ass memory mgmt for now
+// idk malloc later maybe
+
+#include <kernel.h>
+#include <drivers/screen.h>
+
+void init_memory() {
+    print("[*] init_memory(): not doing shit rn lol\n");
+}
