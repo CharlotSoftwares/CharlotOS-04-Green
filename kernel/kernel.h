df4d97521a46c5772076452e7351061802bf0c83
Radiump123 <radiump123@gmail.com>
2025-05-26 18:02:17 -0400

    Create kernel.h

---
+++ kernel/kernel.h
@@ -0,0 +1,11 @@
+// kernel.h — defs for the kernel land
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+void kernel_main();
+void init_system();
+void init_memory();
+void shell_main();  // defined in shell/shell.c
+
+#endif
