f60fdbc8f8d404aac376b88fd7f5d9c530f6c857
Radiump123 <radiump123@gmail.com>
2025-05-26 18:51:26 -0400

    Update keyboard.c

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -1,13 +1,21 @@
-// keyboard.c — keyboard i/o
-// this is just PS/2 magic n stuff
+// keyboard.c — PS/2 scan code based input
 
+#include <stdint.h>
 #include <drivers/keyboard.h>
-#include <drivers/screen.h>
 
 #define KEYBOARD_DATA_PORT 0x60
 
+static const char scancode_to_ascii[] = {
+    0,  27, '1','2','3','4','5','6','7','8','9','0','-','=','\b',
+    '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n', 0,
+    'a','s','d','f','g','h','j','k','l',';','\'','`',  0, '\\',
+    'z','x','c','v','b','n','m',',','.','/', 0, '*', 0, ' ', 0
+};
+
 char get_keypress() {
-    char c;
-    __asm__ __volatile__("inb %1, %0" : "=a"(c) : "Nd"(KEYBOARD_DATA_PORT));
-    return c;
+    uint8_t scancode;
+    do {
+        __asm__ __volatile__("inb %1, %0" : "=a"(scancode) : "Nd"(KEYBOARD_DATA_PORT));
+    } while (scancode & 0x80);  // skip key releases
+    return scancode_to_ascii[scancode];
 }
