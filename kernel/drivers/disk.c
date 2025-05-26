7a5d0af0e95fdb074128fa8ca6b06bf95ee26080
Radiump123 <radiump123@gmail.com>
2025-05-26 18:47:17 -0400

    Create disk.c
    also fake
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
      
      
    son of a bitch

---
+++ kernel/drivers/disk.c
@@ -0,0 +1,12 @@
+// disk.c — block device disk I/O
+// placeholder bs until we touch ATA
+
+#include <drivers/screen.h>
+
+void disk_read_sector(int sector) {
+    print("[disk] lol fake sector read\n");
+}
+
+void disk_write_sector(int sector) {
+    print("[disk] haha write not happening rn\n");
+}
