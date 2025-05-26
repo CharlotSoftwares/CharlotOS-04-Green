92a75887adfd8855c72f434fa1f6ad71eb4e4823
Radiump123 <radiump123@gmail.com>
2025-05-26 18:47:32 -0400

    Create netcard.c
    fuck u

---
+++ kernel/drivers/netcard.c
@@ -0,0 +1,12 @@
+// netcard.c — NIC interface
+// bruh what is networking anyway
+
+#include <drivers/screen.h>
+
+void nic_init() {
+    print("[nic] init: what even is a packet\n");
+}
+
+void nic_send(const char* data) {
+    print("[nic] send: lol nope\n");
+}
