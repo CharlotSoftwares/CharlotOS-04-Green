a03e46d9be5b2d568960e00be991aa8769c097df
Radiump123 <radiump123@gmail.com>
2025-05-27 17:36:31 -0400

    Update ports.c

--- kernel/drivers/ports.c
+++ kernel/drivers/ports.c
@@ -1,9 +1,13 @@
-#ifndef PORTS_H
-#define PORTS_H
+#include "ports.h"
 
-#include <stdint.h>
+// single byte from I/O port
+uint8_t inb(uint16_t port) {
+    uint8_t result;
+    __asm__ volatile ("inb %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
 
-uint8_t inb(uint16_t port);
-void outb(uint16_t port, uint8_t data);
-
-#endif
+// write single byte to I/O port
+void outb(uint16_t port, uint8_t data) {
+    __asm__ volatile ("outb %0, %1" : : "a"(data), "Nd"(port));
+}
